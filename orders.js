07282c109079b48a1b3ca89fb2f851ba41a8b382
danielcheruiyot <danielcheruiyot88@gmail.com>
2023-09-01 14:50:15 +0300

    First commit of responsive-dashboard

---
+++ orders.js
@@ -0,0 +1,23 @@
+const Orders = [ 
+    {
+        productName: 'Javascript tutorial',
+        productNumber: '854777',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '859684',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    
+    },
+    {
+        productName: 'Hosting on Amazon tutorial',
+        productNumber: '854598',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    
+    },
+]
